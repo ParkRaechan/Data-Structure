f7a9e8308c56679bab7983086dfd244b9fae0d22
Dogorithm <0CatThatCat0@gmail.com>
2025-11-08 20:31:55 +0900

    소스코드 커밋
    1차 업데이트

---
+++ 03장 소스코드/HanoiTower.c
@@ -0,0 +1,20 @@
+// 쉽게 배우는 C자료구조(최영규, 생능 출판사, 2024)
+// 참고파일: ch03/HanoiTower.c
+
+#include <stdio.h>
+
+//-------------------------------------------------------------
+// 코드 3.10 하노이의 탑
+#include <stdio.h>
+void hanoi_tower(int n, char from, char tmp, char to)
+{
+    if (n == 1)
+        printf("원판 1: %c --> %c\n", from, to);
+    else {
+        hanoi_tower(n - 1, from, to, tmp);
+        printf("원판 %d: %c --> %c\n", n, from, to);
+        hanoi_tower(n - 1, tmp, from, to);
+    }
+}
+
+void main() { hanoi_tower(4, 'A', 'B', 'C'); }
